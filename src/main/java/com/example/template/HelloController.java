cda916f5d7f800cb371d2acc5efcef7cd084deab
FireBallRun2045 <164293208+FireBallRun2045@users.noreply.github.com>
2025-12-01 19:34:37 -0600

    12/1 from in class. commiting now since forgot how to in class.

--- src/main/java/com/example/template/HelloController.java
+++ src/main/java/com/example/template/HelloController.java
@@ -1,12 +1,8 @@
 package com.example.template;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class HelloController {
@@ -15,7 +11,16 @@ public class HelloController {
     public GridPane gPain;
     private Button[][] btn=new Button[12][12];
     private tile[][] board=new tile[12][12];
+    private int mode=1;
 
+    public void initalize(){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = new tile();
+            }
+        }
+        makeBomb();
+    }
     public void handleClick(ActionEvent actionEvent) {
         for (int i = 0; i < btn.length; i++) {
             for (int j = 0; j < btn[0].length; j++) {
@@ -24,6 +29,8 @@ public class HelloController {
                 //adding it in oppisite order so row,column works.
             }
         }
+
+
         EventHandler z=new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -34,7 +41,7 @@ public class HelloController {
                 int column=GridPane.getColumnIndex(((Button) event.getSource()));
                 System.out.println("("+row+","+column+")");
                 btn[row][column].setText("s");
-                checkBomb(board,column,row);
+                checkBomb(column,row);
             }
         };
 
@@ -45,21 +52,34 @@ public class HelloController {
         }
     }
 
-    public void checkBomb(tile[][] array,int col,int row){
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                if(array[i][j].getBomb()){
-                    System.out.println("("+row+","+col+")"+" is a bomb.");
-                }
-            }
+    public void checkBomb(int col,int row){
+        if(board[row][col].getBomb()){
+            System.out.println("("+row+","+col+")"+" is a bomb.");
+        }
+
+    }
+    public void makeBomb(){
+        int b=1;
+        for (int i = 0; i < b; i++) {
+            int c=random(0,11);
+            int r=random(0,11);
+            board[r][c].setBomb(true);
+            board[r][c].setIfBomborNum(1);
         }
     }
     public void checkNumSurroundingBomb(tile[][] array,int col,int row){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
 
+            }
+        }
     }
 
 
     public int random(int max,int min){
         return (int)(Math.random()*(max-min))+min;
+    }
+    public void setEasy(){
+        mode=1;
     }
 }
--- src/main/java/com/example/template/tile.java
+++ src/main/java/com/example/template/tile.java
@@ -5,8 +5,8 @@ public class tile {
     Boolean revealed=false;
     int num;
     Boolean bomb;
-    public tile(Boolean bomb){
-        this.bomb=bomb;
+    int intstatus=0;
+    public tile(){
     }
     public void updateFlag(Boolean status){
         flagged=status;
@@ -14,6 +14,8 @@ public class tile {
     public Boolean getFlag(){return flagged;}
     public void setBomb(Boolean b){bomb=b;}
     public Boolean getBomb(){return bomb;}
+    public int getIfBomborNum(){return intstatus;}
+    public void setIfBomborNum(int num){intstatus=num;}
     public void updateReveal(Boolean reveal){revealed=reveal;}
     public Boolean getReveal(){return revealed;}
     public void setNum(int n){num=n;}
